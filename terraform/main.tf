89915b0d0319611928430a698f48f05403da7670
Yurii Yevtushenko <gositer2003@gmail.com>
2026-02-25 17:05:53 +0200

    fix: terraform format

--- terraform/main.tf
+++ terraform/main.tf
@@ -23,9 +23,6 @@ module "vpc" {
 }
 
 module "eks" {
-  # trivy:ignore:AVD-AWS-0040
-  # trivy:ignore:AVD-AWS-0041
-  # trivy:ignore:AVD-AWS-0104
   source  = "terraform-aws-modules/eks/aws"
   version = "~> 20.0"
 
@@ -35,8 +32,12 @@ module "eks" {
   vpc_id     = module.vpc.vpc_id
   subnet_ids = module.vpc.private_subnets
 
+  # trivy:ignore:AVD-AWS-0040
+  # trivy:ignore:AVD-AWS-0041
+  cluster_endpoint_public_access  = true
+  cluster_endpoint_private_access = true
+
   enable_cluster_creator_admin_permissions = true
-  cluster_endpoint_public_access           = true
 
   eks_managed_node_groups = {
     general = {

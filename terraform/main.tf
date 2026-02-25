7ad1f3ede49356936b451eece7920f7cbc50f518
Yurii Yevtushenko <gositer2003@gmail.com>
2026-02-25 17:06:49 +0200

    fix: terraform format

--- terraform/main.tf
+++ terraform/main.tf
@@ -32,18 +32,14 @@ module "eks" {
   vpc_id     = module.vpc.vpc_id
   subnet_ids = module.vpc.private_subnets
 
-  # trivy:ignore:AVD-AWS-0040
-  # trivy:ignore:AVD-AWS-0041
-  cluster_endpoint_public_access  = true
-  cluster_endpoint_private_access = true
-
   enable_cluster_creator_admin_permissions = true
+  cluster_endpoint_public_access = true
 
   eks_managed_node_groups = {
     general = {
-      min_size       = var.node_min_size
-      max_size       = var.node_max_size
-      desired_size   = var.node_desired_size
+      min_size     = var.node_min_size
+      max_size     = var.node_max_size
+      desired_size = var.node_desired_size
       instance_types = ["t3.medium"]
       capacity_type  = "SPOT"
     }

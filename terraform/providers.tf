aa3f5ce3692b21d2a2cf0870640d92d5c1a38d6a
Yurii Yevtushenko <gositer2003@gmail.com>
2026-02-26 12:51:41 +0200

    feat: add domain name

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -4,7 +4,16 @@ terraform {
       source  = "hashicorp/aws"
       version = "~> 5.0"
     }
+    helm = {
+      source  = "hashicorp/helm"
+      version = "~> 2.0"
+    }
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = "~> 2.0"
+    }
   }
+
 
   backend "s3" {}
 }

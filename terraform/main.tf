37d26c4b3d0250c31be749f4c111ad626a98452d
Yurii Yevtushenko <gositer2003@gmail.com>
2026-02-26 12:16:46 +0200

    fix: add domain name

--- terraform/main.tf
+++ terraform/main.tf
@@ -64,6 +64,6 @@ module "eks_blueprints_addons" {
   cluster_endpoint  = module.eks.cluster_endpoint
   cluster_version   = module.eks.cluster_version
   oidc_provider_arn = module.eks.oidc_provider_arn
-  
+
   enable_aws_load_balancer_controller = true
 }
